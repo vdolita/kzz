7e9b9eea7a8160a93c8f2fe3a1aafff3ab077974
Vdolita <vdoltia@outlook.com>
2023-03-08 14:52:25 +0800

    init project

---
+++ electron/main.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    if (process.env.VITE_DEV_SERVER_URL) {
+        win.loadURL(process.env.VITE_DEV_SERVER_URL);
+    } else {
+        win.loadFile('dist/index.html');
+    }
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
